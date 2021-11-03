6a08d929345d79710b3356d24ac4f1e2e87b136b
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-11-03 23:49:23 +0530

    Added comment to display current routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,22 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
-  #     articles GET    /articles(.:format)          articles#index
-  #  new_article GET    /articles/new(.:format)      articles#new
-  #      article GET    /articles/:id(.:format)      articles#show
-  #              POST   /articles(.:format)          articles#create
-  # edit_article GET    /articles/:id/edit(.:format) articles#edit
-  #              PATCH  /articles/:id(.:format)      articles#update
-  #              DELETE /articles/:id(.:format)      articles#destroy
 end
+#              Prefix Verb   URI Pattern                                        Controller#Action
+#                root GET    /                                                  articles#index
+#    article_comments GET    /articles/:article_id/comments(.:format)           comments#index
+#                     POST   /articles/:article_id/comments(.:format)           comments#create
+# new_article_comment GET    /articles/:article_id/comments/new(.:format)       comments#new
+# edit_article_comment GET    /articles/:article_id/comments/:id/edit(.:format)  comments#edit
+#     article_comment GET    /articles/:article_id/comments/:id(.:format)       comments#show
+#                     PATCH  /articles/:article_id/comments/:id(.:format)       comments#update
+#                     PUT    /articles/:article_id/comments/:id(.:format)       comments#update
+#                     DELETE /articles/:article_id/comments/:id(.:format)       comments#destroy
+#            articles GET    /articles(.:format)                                articles#index
+#                     POST   /articles(.:format)                                articles#create
+#         new_article GET    /articles/new(.:format)                            articles#new
+#        edit_article GET    /articles/:id/edit(.:format)                       articles#edit
+#             article GET    /articles/:id(.:format)                            articles#show
+#                     PATCH  /articles/:id(.:format)                            articles#update
+#                     PUT    /articles/:id(.:format)                            articles#update
+#                     DELETE /articles/:id(.:format)                            articles#destroy
